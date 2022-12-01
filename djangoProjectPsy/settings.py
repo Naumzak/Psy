771c80edad590b15ead23b05b1db49f275c93283
naum <zakletskiynaum@gmail.com>
2022-12-01 23:45:46 +0200

    Procfile

--- djangoProjectPsy/settings.py
+++ djangoProjectPsy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4nn(y*ylky-))f*yjj4er3ttl188az)^wkws)00+cg$#d$yf&2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
